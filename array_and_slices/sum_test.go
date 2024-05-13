26ca41834998ce85bb660a87862e8955eec28702
iferdel <ignaferdel@gmail.com>
2024-05-13 19:09:16 -0400

    redundant test removed

--- array_and_slices/sum_test.go
+++ array_and_slices/sum_test.go
@@ -5,12 +5,6 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	t.Run("perform Sum with a fixed collection sized array", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
-		got := Sum(numbers)
-		want := 15
-		assertCorrectSum(t, got, want, numbers)
-	})
 	t.Run("perform Sum with a collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 		got := Sum(numbers)
