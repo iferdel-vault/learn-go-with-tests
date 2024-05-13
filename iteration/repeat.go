0ef521e241136980d786e2362770963f401e2371
iferdel <ignaferdel@gmail.com>
2024-05-13 17:07:58 -0400

    specify times the character is repeated in caller

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,8 +1,6 @@
 package iteration
 
-const repeatCount = 4
-
-func Repeat(character string) string {
+func Repeat(character string, repeatCount int) string {
 	repeated := ""
 	for i := 0; i < repeatCount; i++ {
 		repeated += character
