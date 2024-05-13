c84456a9586f27082b204361c6c893ecbacbb20d
iferdel <ignaferdel@gmail.com>
2024-05-12 23:04:26 -0400

    added spanish prefix hello

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -4,16 +4,22 @@ import (
 	"fmt"
 )
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) (helloMessage string) {
+func Hello(name string, language string) (helloMessage string) {
 	if name == "" {
 		name = "World"
 	}
+
+    if language == spanish {
+        return spanishHelloPrefix + name
+    }
 	helloMessage = englishHelloPrefix + name
 	return helloMessage
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
