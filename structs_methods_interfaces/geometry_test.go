e2da9eeaca255153271f8798fcbb700e6f453b01
iferdel <ignaferdel@gmail.com>
2024-05-14 14:44:04 -0400

    refactor tests to better showoff different scenarios by different shapes of Area method

--- structs_methods_interfaces/geometry_test.go
+++ structs_methods_interfaces/geometry_test.go
@@ -5,42 +5,30 @@ import (
 	"testing"
 )
 
-func TestPerimeter(t *testing.T) {
-	t.Run("Rectangles", func(t *testing.T) {
-		rectangle := Rectangle{1.0, 2.0}
-		got := rectangle.Perimeter()
-		want := 6.0
-		assertCorrectComputation(t, got, want)
-	})
-
-	t.Run("Circles", func(t *testing.T) {
-		circle := Circle{2.0}
-		got := circle.Perimeter()
-		want := 2.0 * math.Pi
-		assertCorrectComputation(t, got, want)
-	})
-}
-
 func TestArea(t *testing.T) {
 	// table driven tests
 	areaTest := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-        {shape: Rectangle{Width: 1.0, Height: 2.0}, want: 2.0},
-        {shape: Circle{Radius: 2.0}, want: 2.0 * 2.0 * math.Pi},
-        {shape: Triangle{Base: 6.0, Heigth: 12.0}, want: 36},
+		{name: "Rectangle", shape: Rectangle{Width: 1.0, Height: 2.0}, hasArea: 2.0},
+		{name: "Circle", shape: Circle{Radius: 2.0}, hasArea: 2.0 * 2.0 * math.Pi},
+		{name: "Triangle", shape: Triangle{Base: 6.0, Heigth: 12.0}, hasArea: 36},
 	}
 	for _, tt := range areaTest {
-		got := tt.shape.Area()
-		want := tt.want
-		assertCorrectComputation(t, got, want)
+		// using tt.name for case distinction between shapes in t.Run test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			hasArea := tt.hasArea
+			assertCorrectComputation(t, tt.shape, got, hasArea)
+		})
 	}
 }
 
-func assertCorrectComputation(t testing.TB, got, want float64) {
+func assertCorrectComputation(t testing.TB, shape Shape, got, want float64) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got '%0.2f' want '%0.2f'", got, want)
+		t.Errorf("%#v got '%0.2f' want '%0.2f'", shape, got, want)
 	}
 }
