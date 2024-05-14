cbda338e68b504b3f4920484803e8113d96bf05d
iferdel <ignaferdel@gmail.com>
2024-05-13 23:22:36 -0400

    refactor using append and keeping creating copy of b to avoid buffering dependency and by doing so to be garbaged collected

--- array_and_slices/copydigits.go
+++ array_and_slices/copydigits.go
@@ -9,7 +9,5 @@ func CopyDigitsFromFile(filemame string) []byte {
 	var digitRegexp = regexp.MustCompile("[0-9]+")
 	b, _ := os.ReadFile("./digits.txt")
 	b = digitRegexp.Find(b)
-	c := make([]byte, len(b))
-	copy(c, b)
-	return c
+    return append([]byte{}, b...)
 }
