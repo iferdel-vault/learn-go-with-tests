f39551c1d3c9d5cc0dc97d96fe248e99ebec4ea9
iferdel <ignaferdel@gmail.com>
2024-05-19 12:37:06 -0400

    dict search with map type

---
+++ maps/dictionary_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestDictionary(t *testing.T) {
+	dictionary := map[string]string{"test": "this is a test"}
+	got := Search(dictionary, "test")
+	want := "this is a test"
+
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
